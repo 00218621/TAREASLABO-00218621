3b07bdba87923107b38d614d8e44d516d122c83b
00218621 <90534537+00218621@users.noreply.github.com>
2021-09-24 15:10:29 -0600

    Add files via upload

---
+++ EJERCICIO8GUIA1.cpp
@@ -0,0 +1,35 @@
+#include <iostream> 
+using namespace std;
+void contadorMAS(int);
+void contadorMEN(int);
+void contador(int);
+
+
+void contadorMAS (int num) {
+     
+    if (num < 1)return;
+    contadorMAS (num - 1);
+    cout<<num<<endl;
+}
+void contadorMEN (int num) {
+      if (num < 1 ) return;
+    cout << num << endl;
+    contadorMEN (num - 1);
+}
+void contador(int num){
+  contadorMAS(num);
+  contadorMEN(num);
+}
+
+
+int main()
+{
+    int num=0;
+    cout<<"CONTADOR ASCENDENTE Y DESCENDENTE"<<endl;
+    cout<<"Ingrese el numero entero: "<<endl;
+    cin>>num;
+    cout<<endl;
+    contador(num);
+
+}
+//solo asi lo pude hacer :(
